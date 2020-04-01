4948fbbe43bcc914106c5b9e88b3f911bdb51f5e
limaand <limaand@gmail.com>
2020-04-01 14:41:07 -0300

    criação do prejeto e controllers

---
+++ src/main/java/com/eventos/model/Evento.java
@@ -0,0 +1,82 @@
+package com.eventos.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Evento implements Serializable {
+	
+	private static final long seralVersionUID = 1L;
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long codigo;
+	
+	
+	private String nome;
+	
+	private String local;
+	
+	private String data;
+	
+	private String horario;
+	
+		
+	
+	public Evento() {
+	
+	}
+	
+	
+	
+	
+	public long getCodigo() {
+		return codigo;
+	}
+
+
+
+
+	public void setCodigo(long codigo) {
+		this.codigo = codigo;
+	}
+
+
+
+
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getLocal() {
+		return local;
+	}
+	public void setLocal(String local) {
+		this.local = local;
+	}
+	public String getData() {
+		return data;
+	}
+	public void setData(String data) {
+		this.data = data;
+	}
+	public String getHorario() {
+		return horario;
+	}
+	public void setHorario(String horario) {
+		this.horario = horario;
+	}
+	
+	
+	
+	
+
+}
