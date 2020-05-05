ecb5da80e416b7a10deea4c4cf2667866eeb287a
limaand <limaand@gmail.com>
2020-05-04 21:19:44 -0300

    excluir eventos e validação dos campos

--- src/main/java/com/eventos/controllers/EventoController.java
+++ src/main/java/com/eventos/controllers/EventoController.java
@@ -1,11 +1,15 @@
 package com.eventos.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.eventos.model.Convidado;
 import com.eventos.model.Evento;
@@ -51,21 +55,45 @@ public class EventoController {
 		Evento evento = er.findByCodigo(codigo);
 		ModelAndView mv = new ModelAndView("evento/detalhesEvento");
 		mv.addObject("evento", evento);
+		
+		Iterable<Convidado> convidados = cr.findByEvento(evento);
+		mv.addObject("convidados", convidados);
+		
+		
 		return mv;
 		
 	}
 	
 	
 	@RequestMapping(value="/detalhesEvento/{codigo}", method=RequestMethod.POST )
-	public String detalhesEvento(@PathVariable("codigo") long codigo, Convidado convidado) {
+	public String detalhesEventoPost(@PathVariable("codigo") long codigo, @Valid Convidado convidado, 
+			
+			BindingResult result, RedirectAttributes attributes) {
+		
+		if(result.hasErrors()) {
+			attributes.addFlashAttribute("mensagem", "Verifique os campos!");
+			return "redirect:/detalhesEvento/{codigo}";
+		}
+		
+		
+		
 		Evento evento = er.findByCodigo(codigo);
 		convidado.setEvento(evento);
 		
 		cr.save(convidado);
-		
+		attributes.addFlashAttribute("mensagem", "Convidado adicionado com sucesso!");
 		return "redirect:/detalhesEvento/{codigo}";
 		
 	}
+	
+	@RequestMapping("/deletarEvento/{codigo}")
+	public String deletarEvento(@PathVariable("codigo") long codigo) {
+		Evento evento =  er.findByCodigo(codigo);
+		er.delete(evento);
+		
+		return "redirect:/eventos";
+	}
+	
 	
 	
 	
--- src/main/java/com/eventos/repository/ConvidadoRepository.java
+++ src/main/java/com/eventos/repository/ConvidadoRepository.java
@@ -3,7 +3,10 @@ package com.eventos.repository;
 import org.springframework.data.repository.CrudRepository;
 
 import com.eventos.model.Convidado;
+import com.eventos.model.Evento;
 
 public interface ConvidadoRepository extends CrudRepository<Convidado,String> {
+	
+	Iterable<Convidado> findByEvento(Evento evento);
 
 }
